dad890f18f0945c2692dbae2da4dd02a4f1a644a
Davi Fernando Rosa <davir0514@gmail.com>
2021-06-06 12:06:02 -0300

    Arquivo

---
+++ arraypush.js
@@ -0,0 +1,9 @@
+
+
+
+
+// funcão Nativa para adicionar elemento no   array 
+
+const diasDaSemana = ["domingo", "segunda-feira", "terça-feira",]
+diasDaSemana.push("Quarta");
+console.log(diasDaSemana);
